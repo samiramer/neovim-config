dd7b20f73ec0ddf67ac6aabf677372581d6b582b
Samir Amer <samir@sunnybyte.com>
2022-07-02 12:30:04 -0400

    switch to nightfox theme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -2,6 +2,6 @@ vim.cmd [[
   set background=dark
   let g:gruvbox_contrast_dark = 'hard'
   let g:material_style = "darker"
-  colorscheme gruvbox
-  " colorscheme terafox
+  " colorscheme gruvbox
+  colorscheme nightfox
 ]]
