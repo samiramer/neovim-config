eabd8636cb5aeb5061ca6adc17f7c6aef07a26f6
Samir Amer <samirmamer@gmail.com>
2022-08-30 14:47:55 -0400

    Go back to carbon

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -4,13 +4,14 @@ vim.cmd [[
   " Set contrast.
   " This configuration option should be placed before `colorscheme gruvbox-material`.
   " Available values: 'hard', 'medium'(default), 'soft'
-  let g:gruvbox_material_background = 'hard'
-  let g:gruvbox_material_foreground = 'hard'
+  " let g:gruvbox_material_background = 'hard'
+  " let g:gruvbox_material_foreground = 'original'
   " For better performance
-  let g:gruvbox_material_better_performance = 1
+  " let g:gruvbox_material_better_performance = 1
 
-  colorscheme gruvbox-material
+  " colorscheme gruvbox-material
   " colorscheme terafox
   " colorscheme nord
+  colorscheme carbon
 
 ]]
