795c9f5b9f651b5c64bdb4a11c00c1a0dc4540b0
Samir Amer <samir@sunnybyte.com>
2022-08-30 13:56:53 -0400

    cleanup the colorscheme config, using gruvbox hard

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,17 +1,11 @@
 vim.cmd [[
   set background=dark
 
-  let g:material_style = "default"
-  let g:nord_contrast = v:true
-  let g:nord_borders = v:false
-  let g:nord_disable_background = v:false
-  let g:nord_italic = v:false
-  let g:nord_uniform_diff_background = v:true
-
   " Set contrast.
   " This configuration option should be placed before `colorscheme gruvbox-material`.
   " Available values: 'hard', 'medium'(default), 'soft'
   let g:gruvbox_material_background = 'hard'
+  let g:gruvbox_material_foreground = 'hard'
   " For better performance
   let g:gruvbox_material_better_performance = 1
 
