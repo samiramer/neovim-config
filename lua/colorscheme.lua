fba3135cae9e035604af46a4559db04497574d1f
Samir Amer <samir@sunnybyte.com>
2022-10-08 23:24:29 -0500

    allow transparent backgrounds

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,6 +1,11 @@
 vim.cmd [[
   set background=dark
 
+  augroup user_colors
+    autocmd!
+    autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE
+  augroup END
+
   " Set contrast.
   " This configuration option should be placed before `colorscheme gruvbox-material`.
   " Available values: 'hard', 'medium'(default), 'soft'
