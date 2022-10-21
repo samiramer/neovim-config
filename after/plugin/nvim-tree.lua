e1bb8a8dff458d89bc5474ef271af5b2e9a85be6
Samir Amer <samir@sunnybyte.com>
2022-10-20 23:43:00 -0400

    Configure nvim-tree to float instead pinned to the side

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,22 +1,39 @@
 local status_ok, t = pcall(require, "nvim-tree")
 if not status_ok then return end
 
-t.setup{
-  disable_netrw = true,
-  hijack_netrw = true,
-  filters = {
-    dotfiles = false,
-  },
+t.setup {
+    disable_netrw = true,
+    hijack_netrw = true,
+    filters = {
+        dotfiles = false,
+    },
+    view = {
+        width = 60,
+        float = {
+            enable = true,
+            open_win_config = {
+                width = 80
+            }
+        }
+    },
+    renderer = {
+        indent_markers = {
+            enable = true
+        }
+    },
+    update_focused_file = {
+        enable = true
+    }
 }
 
 local helpers = require('samer.helpers')
 
-helpers.nnoremap("<leader>e", ":NvimTreeToggle<CR>") 
+helpers.nnoremap("<leader>e", ":NvimTreeToggle<CR>")
 
 -- this closes neovim if the nvim-tree buffer is the last one open
 vim.api.nvim_exec(
-  [[
+    [[
     autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
-  ]],
-  false
+  ]] ,
+    false
 )
