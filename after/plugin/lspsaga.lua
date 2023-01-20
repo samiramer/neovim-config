f042a3ce9fc232215f712ea6f9a1629accefc9f5
Samir Amer <samirmamer@gmail.com>
2023-01-20 00:07:17 -0500

    fix: style lspsaga with catppuccin (if the plugin is installed)

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -1,4 +1,22 @@
 local status_ok, saga = pcall(require, "lspsaga")
 if (not status_ok) then return end
 
-saga.init_lsp_saga()
+local has_catppuccin, catppuccin = pcall(require, "catppuccin")
+
+if (not has_catppuccin) then
+    saga.setup {
+        request_timeout = 10000,
+        ui = {
+            border = 'rounded'
+        }
+    }
+else
+    saga.setup {
+        request_timeout = 10000,
+        ui = {
+            border = 'rounded',
+            colors = require("catppuccin.groups.integrations.lsp_saga").custom_colors(),
+            kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
+        }
+    }
+end
