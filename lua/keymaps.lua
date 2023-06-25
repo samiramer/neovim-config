a0ab404924936c58204b27e7732271dbad714508
Samir Amer <samirmamer@gmail.com>
2023-06-25 01:53:24 -0400

    rebuild config with lazy.nvim

---
+++ lua/keymaps.lua
@@ -0,0 +1,39 @@
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ 'n' }, '<leader>h', ':nohlsearch<CR>', { silent = true })
+
+-- Quick escape
+vim.keymap.set('i', 'jk', '<Esc>', { silent = true })
+vim.keymap.set('i', 'kj', '<Esc>', { silent = true })
+
+-- Clipboard copy/paste
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+Y', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P', { silent = true })
+
+-- Stay in indent mode
+vim.keymap.set({ 'v' }, '<', '<gv', { silent = true })
+vim.keymap.set({ 'v' }, '>', '>gv', { silent = true })
+
+-- Window splitting
+vim.keymap.set('n', '<leader>=', ":split<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>-', ":vsplit<CR>", { noremap = true, silent = true })
+
+-- Resize with arrows
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', { silent = true })
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', { silent = true })
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { silent = true })
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { silent = true })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
---
+++ lua/plugins/editor.lua
@@ -0,0 +1,164 @@
+return {
+  -- colorscheme
+  -- {
+  --   'projekt0n/github-nvim-theme',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- Set colorscheme
+  --     vim.o.termguicolors = true
+  --     vim.cmd.colorscheme('github_dark_high_contrast')
+  --   end
+  -- },
+
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    opts = {
+      contrast = 'hard',
+      bold = false,
+      invert_selection = true,
+    },
+    config = function(_, opts)
+      vim.o.termguicolors = true
+      require('gruvbox').setup(opts)
+      vim.cmd.colorscheme('gruvbox')
+    end
+  },
+
+  {
+    'bluz71/vim-nightfly-colors',
+    name = 'nightfly',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.o.termguicolors = true
+      -- vim.cmd.colorscheme('nightfly')
+    end
+  },
+
+  -- file explorer
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    keys = {
+      { '<leader>e', ':NvimTreeFindFileToggle<cr>', { desc = 'Toggle Nvim Tree' } },
+    },
+    opts = {
+      disable_netrw = true,
+      hijack_netrw = true,
+      filters = {
+        dotfiles = false,
+      },
+      view = {
+        width = 50,
+      },
+      git = {
+        ignore = false,
+      },
+      renderer = {
+        group_empty = true,
+        -- icons = {
+        --   show = {
+        --     folder_arrow = false,
+        --   },
+        -- },
+        indent_markers = {
+          enable = true
+        }
+      },
+      update_focused_file = {
+        enable = true
+      }
+    },
+    config = function(_, opts)
+      require('nvim-tree').setup(opts)
+
+      vim.api.nvim_create_autocmd("QuitPre", {
+        callback = function()
+          local invalid_win = {}
+          local wins = vim.api.nvim_list_wins()
+          for _, w in ipairs(wins) do
+            local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+            if bufname:match("NvimTree_") ~= nil then
+              table.insert(invalid_win, w)
+            end
+          end
+          if #invalid_win == #wins - 1 then
+            -- Should quit, so we close all invalid windows.
+            for _, w in ipairs(invalid_win) do vim.api.nvim_win_close(w, true) end
+          end
+        end
+      })
+    end
+  },
+
+  -- nicer buffer delet
+  {
+    'famiu/bufdelete.nvim',
+    keys = {
+      { '<leader>cc', '<cmd>Bdelete<cr>', {} },
+    }
+  },
+
+  { 'christoomey/vim-tmux-navigator' }, -- Easy vim and tmux navigation
+
+  -- utility for commenting
+  {
+    'echasnovski/mini.nvim',
+    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+    config = function()
+      require('mini.pairs').setup({}) -- auto pairs
+      require('mini.comment').setup { -- commenting
+        hooks = {
+          pre = function()
+            require('ts_context_commentstring.internal').update_commentstring({})
+          end,
+        }
+      }
+    end
+  },
+
+  -- buffer display
+  {
+    'akinsho/bufferline.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    event = "VeryLazy",
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        show_close_icon = false,
+        max_name_length = 25,
+        offsets = {
+          {
+            filetype = 'NvimTree',
+            text = '  Files',
+            -- highlight = 'Normal',
+            text_align = 'left',
+          },
+        },
+        custom_areas = {
+          left = function()
+            return {
+              {
+                text = '     ',
+                fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+                bg = vim.api.nvim_get_hl_by_name('Normal', true).background
+              },
+            }
+          end,
+        },
+        highlights = {
+          fill = {
+            bg = vim.api.nvim_get_hl_by_name('Normal', true).background,
+          },
+        }
+      },
+    },
+    keys = {
+      { '<Tab>',   '<cmd>BufferLineCycleNext<cr>', desc = 'Next buffer' },
+      { '<S-Tab>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Previous buffer' },
+    },
+  },
+}
