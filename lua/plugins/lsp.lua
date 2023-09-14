efdf3c986d94a0c244c4d680b82fb8b33311c707
Samir Amer <samirmamer@gmail.com>
2023-09-14 00:10:15 -0400

    switch to a separate linter and formatter plugin setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,31 +9,6 @@ return {
 	},
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		local on_attach = function(_, bufnr)
-			vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-			local opts = { buffer = bufnr }
-			vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
-			vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
-
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
-			vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
-
-			vim.keymap.set("n", "<leader>lD", vim.lsp.buf.type_definition, opts)
-			vim.keymap.set("n", "<leader>lds", require("telescope.builtin").lsp_document_symbols, opts)
-			vim.keymap.set("n", "<leader>lws", require("telescope.builtin").lsp_dynamic_workspace_symbols, opts)
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-			vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
-			vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
-
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-			vim.keymap.set("n", "<leader>lf", function()
-				vim.lsp.buf.format({ timeout_ms = 5000 })
-			end, opts)
-		end
-
 		require("mason").setup()
 		require("mason-lspconfig").setup({
 			automatic_installation = true,
@@ -44,7 +19,6 @@ return {
 
 		local lspconfig = require("lspconfig")
 		lspconfig.intelephense.setup({
-			on_attach = on_attach,
 			settings = {
 				maxMemory = 4096,
 				format = {
@@ -54,7 +28,6 @@ return {
 		})
 
 		lspconfig.lua_ls.setup({
-			on_attach = on_attach,
 			settings = {
 				Lua = {
 					workspace = { checkThirdParty = false },
@@ -64,38 +37,32 @@ return {
 		})
 
 		lspconfig.volar.setup({
-			on_attach = on_attach,
 			settings = {
 				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 			},
 		})
 
-		-- Register linters and formatters per language
-		local eslint = require("efmls-configs.linters.eslint")
-		local eslint_fmt = require("efmls-configs.formatters.eslint")
-		local prettier = require("efmls-configs.formatters.prettier")
-		local stylua = require("efmls-configs.formatters.stylua")
-		local phpcsfixer = require("efmls-configs.formatters.php_cs_fixer")
+		-- Register linters language
+		local eslint = require("efmls-configs.linters.eslint_d")
 		local languages = {
-			typescript = { eslint, eslint_fmt, prettier },
-			typescriptreact = { eslint, eslint_fmt, prettier },
-			javascript = { eslint, eslint_fmt, prettier },
-			javascriptreact = { eslint, eslint_fmt, prettier },
-			vue = { eslint, eslint_fmt, prettier },
-			php = { phpcsfixer },
-			lua = { stylua },
+			typescript = { eslint },
+			typescriptreact = { eslint },
+			javascript = { eslint },
+			javascriptreact = { eslint },
+			vue = { eslint },
 		}
 
+		-- Use efm for linting
 		lspconfig.efm.setup({
-			on_attach = on_attach,
 			filetypes = vim.tbl_keys(languages),
 			settings = {
 				rootMarkers = { ".git/" },
 				languages = languages,
 			},
 			init_options = {
-				documentFormatting = true,
-				documentRangeFormatting = true,
+				-- don't use efm for formatting
+				documentFormatting = false,
+				documentRangeFormatting = false,
 				hover = true,
 				documentSymbol = true,
 				codeAction = true,
@@ -103,64 +70,67 @@ return {
 			},
 		})
 
-		--		local function prettier_package_json_key_exists(project_root)
-		--			local ok, has_prettier_key = pcall(function()
-		--				local package_json_blob =
-		--					table.concat(vim.fn.readfile(require("lspconfig.util").path.join(project_root, "/package.json")))
-		--				local package_json = vim.json.decode(package_json_blob) or {}
-		--				return not not package_json.prettier
-		--			end)
-		--			return ok and has_prettier_key
-		--		end
-		--
-		--		local function prettier_config_file_exists(project_root)
-		--			return (not not project_root) and vim.tbl_count(vim.fn.glob(".prettierrc*", true, true)) > 0
-		--				or vim.tbl_count(vim.fn.glob("prettier.config.*", true, true)) > 0
-		--		end
-		--
-		--		local function prettier()
-		--			local startpath = vim.fn.getcwd()
-		--			local project_root = (
-		--				require("lspconfig.util").find_git_ancestor(startpath)
-		--				or require("lspconfig.util").find_package_json_ancestor(startpath)
-		--			)
-		--			if prettier_config_file_exists(project_root) or prettier_package_json_key_exists(project_root) then
-		--				return require("formatter.defaults.prettier")()
-		--			end
-		--		end
-		--
-		--		require("formatter").setup({
-		--			filetype = {
-		--				lua = {
-		--					require("formatter.filetypes.lua").stylua,
-		--				},
-		--				php = {
-		--					require("formatter.filetypes.php").php_cs_fixer,
-		--				},
-		--				javascript = {
-		--					require("formatter.filetypes.javascript").eslint_d,
-		--					prettier,
-		--				},
-		--				javascriptreact = {
-		--					require("formatter.filetypes.javascript").eslint_d,
-		--					prettier,
-		--				},
-		--				typescript = {
-		--					require("formatter.filetypes.typescript").eslint_d,
-		--					prettier,
-		--				},
-		--				typescriptreact = {
-		--					require("formatter.filetypes.typescript").eslint_d,
-		--					prettier,
-		--				},
-		--				vue = {
-		--					require("formatter.filetypes.typescript").eslint_d,
-		--					prettier,
-		--        }
-		--			},
-		--		})
-		--
-		--    vim.keymap.set("n", "<leader>lf", ":Format<cr>")
+		local function prettier_package_json_key_exists(project_root)
+			local ok, has_prettier_key = pcall(function()
+				local package_json_blob =
+					table.concat(vim.fn.readfile(require("lspconfig.util").path.join(project_root, "/package.json")))
+				local package_json = vim.json.decode(package_json_blob) or {}
+				return not not package_json.prettier
+			end)
+			return ok and has_prettier_key
+		end
+
+		local function prettier_config_file_exists(project_root)
+			return (not not project_root) and vim.tbl_count(vim.fn.glob(".prettierrc*", true, true)) > 0
+				or vim.tbl_count(vim.fn.glob("prettier.config.*", true, true)) > 0
+		end
+
+		local function prettier()
+			local startpath = vim.fn.getcwd()
+			local project_root = (
+				require("lspconfig.util").find_git_ancestor(startpath)
+				or require("lspconfig.util").find_package_json_ancestor(startpath)
+			)
+			if prettier_config_file_exists(project_root) or prettier_package_json_key_exists(project_root) then
+				return require("formatter.defaults.prettier")()
+			end
+		end
+
+		require("formatter").setup({
+			filetype = {
+				lua = {
+					require("formatter.filetypes.lua").stylua,
+				},
+				php = {
+					require("formatter.filetypes.php").php_cs_fixer,
+				},
+				javascript = {
+					prettier,
+					require("formatter.filetypes.javascript").eslint_d,
+				},
+				javascriptreact = {
+					prettier,
+					require("formatter.filetypes.javascript").eslint_d,
+				},
+				typescript = {
+					prettier,
+					require("formatter.filetypes.typescript").eslint_d,
+				},
+				typescriptreact = {
+					prettier,
+					require("formatter.filetypes.typescript").eslint_d,
+				},
+				vue = {
+					prettier,
+					require("formatter.filetypes.typescript").eslint_d,
+				},
+				twig = {
+					prettier,
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>lf", ":Format<cr>")
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -184,9 +154,6 @@ return {
 				vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
 
 				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-				vim.keymap.set("n", "<leader>lf", function()
-					vim.lsp.buf.format({ timeout_ms = 5000 })
-				end, opts)
 			end,
 		})
 	end,
