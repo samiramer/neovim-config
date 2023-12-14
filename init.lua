a3b16a420d7eca13c2364d0796bf6a57d0b25038
Samir Amer <samir@sunnybyte.com>
2023-12-15 01:17:14 +0400

    Switch over to catpuccin

--- init.lua
+++ init.lua
@@ -2,3 +2,7 @@ require("options")
 require("keymaps")
 require("core")
 require("statusline")
+
+vim.o.termguicolors = true
+vim.o.background = "dark"
+vim.cmd.colorscheme("catppuccin")
--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,18 +1,12 @@
 return {
-  -- colorscheme
-  {
-    'ellisonleao/gruvbox.nvim',
-    priority = 1000,
-    opts = {
-      contrast = 'hard',
-      bold = false,
-      invert_selection = true,
-    },
-    config = function(_, opts)
-      vim.o.termguicolors = true
-      vim.o.background = 'dark'
-      require('gruvbox').setup(opts)
-      vim.cmd.colorscheme('gruvbox')
-    end
-  },
+	-- colorscheme
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		opts = {
+			contrast = "hard",
+			bold = false,
+			invert_selection = true,
+		},
+	},
 }
