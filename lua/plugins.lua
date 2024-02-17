f27e581b2e18e93ea4d76a7d05e9e4ffce310ce9
Samir Amer <samir@sunnybyte.com>
2024-02-16 22:30:37 -0500

    Install which key plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -820,6 +820,19 @@ local plugins = {
 			}
 		end,
 	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
 
 require("lazy").setup(plugins, opts)
