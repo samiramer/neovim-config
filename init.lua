0604aa698981bb9f4b2249979c6015f13fc2e3fc
Samir Amer <samir@sunnybyte.com>
2024-03-20 01:18:44 -0400

    Install DAP plugin and config

--- init.lua
+++ init.lua
@@ -369,6 +369,7 @@ local plugins = {
 				"eslint_d",
 				"phpcs",
 				"phpstan",
+				"php-debug-adapter",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
@@ -573,6 +574,171 @@ local plugins = {
 					lint.try_lint()
 				end,
 			})
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"theHamsta/nvim-dap-virtual-text",
+			{
+				"nvim-telescope/telescope-dap.nvim",
+				dependencies = { "nvim-telescope/telescope.nvim" },
+				config = function()
+					require("telescope").load_extension("dap")
+				end,
+			},
+			{
+				"jay-babu/mason-nvim-dap.nvim",
+				dependencies = "mason.nvim",
+				cmd = { "DapInstall", "DapUninstall" },
+				opts = {
+					automatic_installation = true,
+					ensure_installed = { "php" },
+				},
+			},
+			{
+				"rcarriga/nvim-dap-ui",
+				keys = {
+					{
+						"<leader>du",
+						function()
+							require("dapui").toggle({})
+						end,
+						desc = "[D]ap [U]I",
+					},
+					{
+						"<leader>de",
+						function()
+							require("dapui").eval()
+						end,
+						desc = "[D]AP [e]val",
+						mode = { "n", "v" },
+					},
+				},
+				opts = {},
+				config = function(_, opts)
+					local dap = require("dap")
+					local dapui = require("dapui")
+					dapui.setup(opts)
+					dap.listeners.after.event_initialized["dapui_config"] = function()
+						dapui.open({})
+					end
+					dap.listeners.before.event_terminated["dapui_config"] = function()
+						dapui.close({})
+					end
+					dap.listeners.before.event_exited["dapui_config"] = function()
+						dapui.close({})
+					end
+				end,
+			},
+		},
+		keys = {
+			{
+				"<leader>db",
+				function()
+					require("dap").toggle_breakpoint()
+				end,
+				desc = "[D]AP Toggle [b]reakpoint",
+			},
+			{
+				"<leader>dp",
+				function()
+					require("dap").list_breakpoints()
+				end,
+				desc = "[D]AP List [b]reakpoints",
+			},
+			{
+				"<leader>dC",
+				function()
+					require("dap").clear_breakpoints()
+				end,
+				desc = "[D]AP [C]lear Breakpoints",
+			},
+			{
+				"<leader>dc",
+				function()
+					require("dap").continue()
+				end,
+				desc = "[D]AP [c]ontinue",
+			},
+			{
+				"<leader>dl",
+				function()
+					require("dap").run_last()
+				end,
+				desc = "[D]AP Run [l]ast debugger",
+			},
+			{
+				"<leader>do",
+				function()
+					require("dap").step_over()
+				end,
+				desc = "[D]AP Step [o]ver",
+			},
+			{
+				"<leader>dO",
+				function()
+					require("dap").step_out()
+				end,
+				desc = "[D]AP Step [O]ut",
+			},
+			{
+				"<leader>di",
+				function()
+					require("dap").step_into()
+				end,
+				desc = "[D]AP Step [i]nto",
+			},
+			{
+				"<leader>dt",
+				function()
+					require("dap").terminate()
+				end,
+				desc = "[D]AP [t]erminate",
+			},
+			{
+				"<leader>dR",
+				function()
+					require("dap").restart()
+				end,
+				desc = "[D]AP [R]estart",
+			},
+			{
+				"<leader>dr",
+				function()
+					require("dap.repl").toggle()
+				end,
+				desc = "[D]AP Toggle [r]epl",
+			},
+			{
+				"<leader>dh",
+				function()
+					require("dap.ui.widgets").hover()
+				end,
+				desc = "[D]AP [H]over",
+			},
+		},
+		opts = {},
+		config = function()
+			local dap = require("dap")
+
+			dap.adapters.php = {
+				type = "executable",
+				command = "php-debug-adapter",
+			}
+
+			dap.configurations.php = {
+				{
+					type = "php",
+					request = "launch",
+					name = "PHP: Listen for Xdebug",
+					hostname = "0.0.0.0",
+					port = 9003,
+					pathMappings = {
+						["/var/www/html"] = "${workspaceFolder}",
+					},
+				},
+			}
 		end,
 	},
 }
