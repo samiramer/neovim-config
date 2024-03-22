fd013bec30e89147f336b355ba3d144e57bc72fd
Samir Amer <samir@sunnybyte.com>
2024-03-22 01:06:27 -0400

    Remove markdown_inline TSitter module and increase file limit for disabling TSitter

--- init.lua
+++ init.lua
@@ -434,7 +434,6 @@ local plugins = {
 				"luadoc",
 				"luap",
 				"markdown",
-				"markdown_inline",
 				"php",
 				"tsx",
 				"twig",
@@ -448,7 +447,7 @@ local plugins = {
 			highlight = {
 				enable = true,
 				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
+					local max_filesize = 1000 * 1024 -- 1 MB
 					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 					if ok and stats and stats.size > max_filesize then
 						return true
