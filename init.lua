4bb8ec46436c72d67d9bd496afbd6c92b910fc70
Samir Amer <samir@sunnybyte.com>
2024-04-12 19:34:18 -0400

    Open nvim-tree on the side

--- init.lua
+++ init.lua
@@ -830,14 +830,14 @@ local plugins = {
 			{
 				"<leader>e",
 				function()
-					require("nvim-tree.api").tree.toggle({ "find_file = true" })
+					require("nvim-tree.api").tree.toggle({ find_file = true })
 				end,
 				{ desc = "Launch file [e]xplorer" },
 			},
 		},
 		config = function()
 			local HEIGHT_RATIO = 0.8 -- You can change this
-			local WIDTH_RATIO = 0.5
+			local WIDTH_RATIO = 0.25
 			require("nvim-tree").setup({
 				disable_netrw = true,
 				hijack_netrw = true,
@@ -851,30 +851,33 @@ local plugins = {
 					group_empty = true,
 				},
 				view = {
-					float = {
-						enable = true,
-						open_win_config = function()
-							local screen_w = vim.opt.columns:get()
-							local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-							local window_w = screen_w * WIDTH_RATIO
-							local window_h = screen_h * HEIGHT_RATIO
-							local window_w_int = math.floor(window_w)
-							local window_h_int = math.floor(window_h)
-							local center_x = (screen_w - window_w) / 2
-							local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
-							return {
-								border = "rounded",
-								relative = "editor",
-								row = center_y,
-								col = center_x,
-								width = window_w_int,
-								height = window_h_int,
-							}
-						end,
-					},
 					width = function()
 						return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
 					end,
+					-- float = {
+					-- 	enable = true,
+					-- 	open_win_config = function()
+					-- 		local screen_w = vim.opt.columns:get()
+					-- 		local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+					-- 		local window_w = screen_w * WIDTH_RATIO
+					-- 		local window_h = screen_h * HEIGHT_RATIO
+					-- 		local window_w_int = math.floor(window_w)
+					-- 		local window_h_int = math.floor(window_h)
+					-- 		local center_x = (screen_w - window_w) / 2
+					-- 		local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+					-- 		return {
+					-- 			border = "rounded",
+					-- 			relative = "editor",
+					-- 			row = center_y,
+					-- 			col = center_x,
+					-- 			width = window_w_int,
+					-- 			height = window_h_int,
+					-- 		}
+					-- 	end,
+					-- },
+					-- width = function()
+					-- 	return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+					-- end,
 				},
 			})
 
