59f41ac1bc8320a43f27f16fbb2bcdcc570e7cab
Samir Amer <samir@sunnybyte.com>
2024-04-24 15:43:54 -0400

    Simplify the lualine config

--- init.lua
+++ init.lua
@@ -1018,8 +1018,8 @@ local plugins = {
 				options = {
 					icons_enabled = true,
 					theme = "auto",
-					component_separators = { left = "", right = "" },
-					section_separators = { left = "", right = "" },
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
 					disabled_filetypes = {
 						statusline = {},
 						winbar = {},
