21aa30215de1138b741c0134930a78a2aeb97af2
Samir Amer <samir@sunnybyte.com>
2024-04-24 15:44:09 -0400

    Tell nvim-tree to auto focus to opened buffer file

--- init.lua
+++ init.lua
@@ -797,6 +797,9 @@ local plugins = {
 			require("nvim-tree").setup({
 				disable_netrw = true,
 				hijack_netrw = true,
+				update_focused_file = {
+					enable = true,
+				},
 				filters = {
 					dotfiles = false,
 				},
