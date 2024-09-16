6eee46b91bea4d7b02073030a4dbe74d853cd048
Samir Amer <samir@sunnybyte.com>
2024-09-16 17:11:22 +0400

    Add blade treesitter support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
 		opts = {
 			ensure_installed = {
 				"bash",
+				"blade",
 				"lua",
 				"html",
 				"javascript",
@@ -37,7 +38,22 @@ return {
 			},
 		},
 		config = function(_, opts)
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.blade = {
+				install_info = {
+					url = "https://github.com/EmranMR/tree-sitter-blade",
+					files = { "src/parser.c" },
+					branch = "main",
+				},
+				filetype = "blade",
+			}
 			require("nvim-treesitter.configs").setup(opts)
+
+			vim.filetype.add({
+				pattern = {
+					['.*%.blade%.php'] = 'blade'
+				}
+			})
 		end,
 	},
 	{ -- context based commenting support
