7d03e5a546f65b4da7553977e56866283abd89e8
Samir Amer <samir@sunnybyte.com>
2024-10-04 16:08:49 +0400

    Install lualine, replace mini status line

--- lua/plugins/utilities.lua
+++ lua/plugins/utilities.lua
@@ -19,10 +19,36 @@ return {
 
 	-- statusline
 	{
-		"echasnovski/mini.statusline",
-		version = false,
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("mini.statusline").setup()
+			require("lualine").setup({
+				options = {
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {"NvimTree"},
+					},
+					always_divide_middle = true,
+					globalstatus = true,
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "filename" },
+					lualine_c = { "diff", "diagnostics" },
+					lualine_x = { "branch", "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_a = { "filename" },
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = { "location" },
+					lualine_y = {},
+					lualine_z = {},
+				},
+			})
 		end,
 	},
 }
